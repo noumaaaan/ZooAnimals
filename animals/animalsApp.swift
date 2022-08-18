b54a537a421cb02f1e8b53f3d37b331bb1074e8a
Nouman Mehmood <nmehmood@pixsystem.com>
2022-08-18 10:20:23 +0100

    Initial Commit

---
+++ animals/animalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animalsApp.swift
+//  animals
+//
+//  Created by Nouman Mehmood on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct animalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
